0de1dbb5104d8145e849b9833d12906d44384d6f
Susan Kim <suzenyada@gmail.com>
2021-08-31 14:41:07 +0900

    Initial Commit

---
+++ AppleWatchStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleWatchStore
+//
+//  Created by Susan Kim on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
